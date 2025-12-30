e31b7bcd52672cb31e25bed5b1590fd299033055
sidit77 <sidit77@gmail.com>
2025-12-30 14:08:32 +0100

    Project Setup

---
+++ src/client/java/com/github/sidit77/perfect_presentation/client/PerfectPresentationClient.java
@@ -0,0 +1,11 @@
+package com.github.sidit77.perfect_presentation.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PerfectPresentationClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/github/sidit77/perfect_presentation/PerfectPresentation.java
@@ -0,0 +1,10 @@
+package com.github.sidit77.perfect_presentation;
+
+import net.fabricmc.api.ModInitializer;
+
+public class PerfectPresentation implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
