14ba5cccec2fb8e28a1b3e40bdbcdbc786e941b7
sidit77 <sidit77@gmail.com>
2025-12-31 13:11:16 +0100

    some work on implementing shared textures

--- native/perfect_presentation_core/src/lib.rs
+++ native/perfect_presentation_core/src/lib.rs
@@ -1,13 +1,17 @@
 mod wgl;
 
+use std::cell::Cell;
+use std::collections::BTreeMap;
 use std::ops::Deref;
+use std::sync::{Arc, Weak};
 use windows::Win32::Foundation::HMODULE;
 use windows::Win32::UI::WindowsAndMessaging::GetClientRect;
 
-use crate::wgl::{DxDeviceHandle, Wgl, CONTEXT_CORE_PROFILE_BIT_ARB, CONTEXT_FLAGS_ARB, CONTEXT_FORWARD_COMPATIBLE_BIT_ARB, CONTEXT_MAJOR_VERSION_ARB, CONTEXT_MINOR_VERSION_ARB, CONTEXT_PROFILE_MASK_ARB};
+use crate::wgl::{DxDeviceHandle, DxResourceHandle, GLenum, GLuint, Wgl, ACCESS_READ_WRITE_NV, CONTEXT_CORE_PROFILE_BIT_ARB, CONTEXT_FLAGS_ARB, CONTEXT_FORWARD_COMPATIBLE_BIT_ARB, CONTEXT_MAJOR_VERSION_ARB, CONTEXT_MINOR_VERSION_ARB, CONTEXT_PROFILE_MASK_ARB};
 pub use windows::Win32::Foundation::HWND;
-use windows::Win32::Graphics::Direct3D11::{D3D11CreateDevice, ID3D11Device, ID3D11DeviceContext, D3D11_CREATE_DEVICE_BGRA_SUPPORT, D3D11_CREATE_DEVICE_DEBUG, D3D11_SDK_VERSION};
+use windows::Win32::Graphics::Direct3D11::{D3D11CreateDevice, ID3D11Device, ID3D11DeviceContext, ID3D11Texture2D, D3D11_BIND_RENDER_TARGET, D3D11_CREATE_DEVICE_BGRA_SUPPORT, D3D11_CREATE_DEVICE_DEBUG, D3D11_SDK_VERSION, D3D11_TEXTURE2D_DESC, D3D11_USAGE_DEFAULT};
 use windows::Win32::Graphics::Direct3D::{D3D_DRIVER_TYPE_HARDWARE, D3D_FEATURE_LEVEL_11_1};
+use windows::Win32::Graphics::Dxgi::Common::{DXGI_FORMAT_B8G8R8A8_UNORM, DXGI_SAMPLE_DESC};
 use windows::Win32::Graphics::Gdi::{GetDC, ReleaseDC, HDC};
 use windows::Win32::Graphics::OpenGL::{wglCreateContext, wglDeleteContext, wglMakeCurrent, ChoosePixelFormat, DescribePixelFormat, SetPixelFormat, SwapBuffers, HGLRC, PFD_DOUBLEBUFFER, PFD_DRAW_TO_WINDOW, PFD_MAIN_PLANE, PFD_SUPPORT_OPENGL, PFD_TYPE_RGBA, PIXELFORMATDESCRIPTOR};
 
@@ -103,7 +107,7 @@ impl WglContext {
 
 impl Drop for WglContext {
     fn drop(&mut self) {
-        unsafe { 
+        unsafe {
             wglMakeCurrent(self.dc, HGLRC::default()).unwrap();
             wglDeleteContext(self.rc).unwrap();
             ReleaseDC(Some(self.hwnd), self.dc);
@@ -122,7 +126,8 @@ pub struct InteropContext {
     pub wgl: WglContext,
     pub device: ID3D11Device,
     pub device_context: ID3D11DeviceContext,
-    pub interop_handle: DxDeviceHandle
+    pub interop_handle: DxDeviceHandle,
+    pub shared_textures: BTreeMap<GLuint, SharedTexture>
 }
 
 impl InteropContext {
@@ -145,15 +150,31 @@ impl InteropContext {
             ).unwrap();
             (device.unwrap(), device_context.unwrap())
         };
-        
+
         let interop_handle = unsafe { wgl.DXOpenDeviceNV(&device).unwrap() };
-        
-        Self { wgl, device, device_context, interop_handle }
+
+        Self { wgl, device, device_context, interop_handle, shared_textures: Default::default() }
     }
+
+    pub fn create_shared_texture(&mut self, gl_ident: GLuint) -> &SharedTexture {
+        todo!()
+    }
+
+    pub fn get_shared_texture(&self, gl_ident: GLuint) -> &SharedTexture {
+        self.shared_textures.get(&gl_ident).expect("Shared texture not found")
+    }
+
+    pub fn delete_shared_texture(&mut self, gl_ident: GLuint) {
+        self.shared_textures.remove(&gl_ident).expect("Shared texture not found");
+    }
+
 }
 
 impl Drop for InteropContext {
     fn drop(&mut self) {
+        unsafe { self.device_context.ClearState(); }
+        SharedTexture::unlock(self.shared_textures.values());
+        self.shared_textures.clear();
         unsafe { self.wgl.functions.DXCloseDeviceNV(self.interop_handle).unwrap(); }
         self.interop_handle = Default::default();
     }
@@ -161,3 +182,91 @@ impl Drop for InteropContext {
 
 unsafe impl Send for InteropContext {}
 unsafe impl Sync for InteropContext {}
+
+pub struct SharedTexture {
+    context: Weak<InteropContext>,
+    texture: ID3D11Texture2D,
+    interop_handle: DxResourceHandle,
+    locked: Cell<bool>
+}
+
+impl SharedTexture {
+
+    fn new(context: &Arc<InteropContext>, gl_ident: GLuint, width: u32, height: u32) -> Self {
+        let texture = unsafe {
+            let mut temp = None;
+            context.device.CreateTexture2D(&D3D11_TEXTURE2D_DESC {
+                Width: width,
+                Height: height,
+                MipLevels: 1,
+                ArraySize: 1,
+                Format: DXGI_FORMAT_B8G8R8A8_UNORM,
+                SampleDesc: DXGI_SAMPLE_DESC { Count: 1, Quality: 0 },
+                Usage: D3D11_USAGE_DEFAULT,
+                BindFlags: D3D11_BIND_RENDER_TARGET.0 as _,
+                CPUAccessFlags: 0,
+                MiscFlags: 0,
+            }, None, Some(&mut temp)).unwrap();
+            temp.unwrap()
+        };
+
+        let interop_handle = unsafe {
+            pub const RENDERBUFFER: GLenum = 0x8D41;
+            context.wgl.DXRegisterObjectNV(context.interop_handle, &texture, gl_ident, RENDERBUFFER, ACCESS_READ_WRITE_NV).unwrap()
+        };
+
+        Self {
+            context: Arc::downgrade(context),
+            texture,
+            interop_handle,
+            locked: Cell::new(false),
+        }
+    }
+
+    fn context(&self) -> Arc<InteropContext> {
+        self.context.upgrade().expect("SharedTexture outlived its context")
+    }
+
+    fn update_lock_status<'a, I: IntoIterator<Item = &'a Self>>(textures: I, locked: bool) {
+        let mut handles = heapless::Vec::<DxResourceHandle, 16>::new();
+        let mut lock_status = heapless::Vec::<&Cell<bool>, 16>::new();
+        let mut context = None;
+        for texture in textures {
+            assert_ne!(texture.locked.get(), locked);
+            assert_eq!(Arc::as_ptr(context.get_or_insert_with(|| texture.context())), texture.context.as_ptr(), "Not all textures belong to the same context");
+            handles.push(texture.interop_handle).expect("Can only lock 16 textures at once");
+            lock_status.push(&texture.locked).unwrap();
+        }
+        if let Some(context) = context {
+            if locked {
+                unsafe { context.wgl.functions.DXLockObjectsNV(context.interop_handle, handles.as_slice()).unwrap(); }
+            } else {
+                unsafe { context.wgl.functions.DXUnlockObjectsNV(context.interop_handle, handles.as_slice()).unwrap(); }
+            }
+            for status in lock_status {
+                status.set(locked);
+            }
+        }
+    }
+
+    pub fn lock<'a, I: IntoIterator<Item = &'a Self>>(textures: I) {
+        SharedTexture::update_lock_status(textures, true);
+    }
+
+    pub fn unlock<'a, I: IntoIterator<Item = &'a Self>>(textures: I) {
+        SharedTexture::update_lock_status(textures, false);
+    }
+
+}
+
+impl Drop for SharedTexture {
+    fn drop(&mut self) {
+        let context = self.context();
+        unsafe {
+            if self.locked.get() {
+                context.wgl.functions.DXUnlockObjectsNV(context.interop_handle, &[self.interop_handle]).unwrap();
+            }
+            context.wgl.functions.DXUnregisterObjectNV(context.interop_handle, self.interop_handle).unwrap();
+        }
+    }
+}
