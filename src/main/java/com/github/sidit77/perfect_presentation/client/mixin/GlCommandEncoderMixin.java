1483f116c9a6e8afb321d5754e3152bbaa59a0e4
sidit77 <sidit77@gmail.com>
2026-04-28 09:46:12 +0200

    Finally fixed the stupid gl errors when the blur shader was used

--- src/main/java/com/github/sidit77/perfect_presentation/client/mixin/GlCommandEncoderMixin.java
+++ src/main/java/com/github/sidit77/perfect_presentation/client/mixin/GlCommandEncoderMixin.java
@@ -2,8 +2,12 @@ package com.github.sidit77.perfect_presentation.client.mixin;
 
 import com.github.sidit77.perfect_presentation.client.GpuDeviceExtensions;
 import com.github.sidit77.perfect_presentation.client.SharedGlTexture;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.opengl.GlCommandEncoder;
 import com.mojang.blaze3d.opengl.GlDevice;
+import com.mojang.blaze3d.opengl.GlTexture;
 import com.mojang.blaze3d.textures.GpuTexture;
 import com.mojang.blaze3d.textures.GpuTextureView;
 import org.spongepowered.asm.mixin.Final;
@@ -12,6 +16,9 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static org.lwjgl.opengl.GL12.GL_TEXTURE_BASE_LEVEL;
+import static org.lwjgl.opengl.GL12.GL_TEXTURE_MAX_LEVEL;
 
 @Mixin(GlCommandEncoder.class)
 public class GlCommandEncoderMixin {
@@ -32,6 +39,23 @@ public class GlCommandEncoderMixin {
         GpuDeviceExtensions ext = (GpuDeviceExtensions)device;
         ext.perfect_presentation$blitSharedTextureToSwapChain((SharedGlTexture) gpuTextureView.texture());
         ci.cancel();
+    }
+
+    // Calling glTexParameteri with either GL_TEXTURE_MAX_LEVEL or GL_TEXTURE_BASE_LEVEL on a D3D11 backed texture
+    // causes a bunch of GL_INVALID_OPERATION errors in strange places, and setting the
+    // D3D11_RESOURCE_MISC_RESOURCE_CLAMP flag when creating the D3D11 texture breaks the creation of the interop
+    // object.
+    //
+     // To fix this, we just disable the LOD clamping for shared textures.
+    @WrapOperation(
+            method = "trySetup(Lcom/mojang/blaze3d/opengl/GlRenderPass;Ljava/util/Collection;)Z",
+            at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/opengl/GlStateManager;_texParameter(III)V")
+    )
+    void preventLodClamping(int i, int j, int k, Operation<Void> original, @Local GlTexture glTexture) {
+        boolean isSharedTexture = glTexture instanceof SharedGlTexture;
+        if(isSharedTexture && (j == GL_TEXTURE_MAX_LEVEL || j == GL_TEXTURE_BASE_LEVEL))
+            return;
+        original.call(i, j, k);
     }
 
 }
